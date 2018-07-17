a8e6501202a2d44ab0a1e02cebd2310bd7e486bd
Rafael Rodriguez <rrodriguez1989@gmail.com>
2018-07-16 20:29:07 -0500

    Added return home action to dropdown menu

--- GradQuest/src/application/controller/IntroController.java
+++ GradQuest/src/application/controller/IntroController.java
@@ -1,11 +1,17 @@
 package application.controller;
 
+import java.io.IOException;
+
+import application.Main;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 /**
@@ -29,7 +35,16 @@ public class IntroController implements EventHandler<ActionEvent>{
     
     @Override
     public void handle(ActionEvent event) {
-        // TODO Return the user to the main menu
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../Main.fxml"));
+            AnchorPane layout = (AnchorPane) loader.load();
+            Scene scene = new Scene(layout);
+            Main.stage.setScene(scene);
+            Main.stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     public void exitGame() {
