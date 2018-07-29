e5a54dcf08d97c63ed457b9435abe07898d876e4
MLG4Scriptin <davidbren@gmail.com>
2018-07-29 15:20:43 -0500

    Adding constants for file images
    initalizing player animations

---
+++ GradQuest/src/application/constants/PlayerAnimationConstants.java
@@ -0,0 +1,20 @@
+package application.constants;
+
+public class PlayerAnimationConstants {
+
+	public static final String PLAYER_DOWN0 = "down0.png";
+	public static final String PLAYER_DOWN1 = "down1.png";
+	public static final String PLAYER_DOWN2 = "down2.png";
+	
+	public static final String PLAYER_UP0 = "up0.png";
+	public static final String PLAYER_UP1 = "up1.png";
+	public static final String PLAYER_UP2 = "up2.png";
+	
+	public static final String PLAYER_RIGHT0 = "right0.png";
+	public static final String PLAYER_RIGHT1 = "right1.png";
+	public static final String PLAYER_RIGHT2 = "right2.png";
+	
+	public static final String PLAYER_LEFT0 = "left0.png";
+	public static final String PLAYER_LEFT1 = "left1.png";
+	public static final String PLAYER_LEFT2 = "left2.png";
+}
--- GradQuest/src/application/controller/GameController.java
+++ GradQuest/src/application/controller/GameController.java
@@ -3,17 +3,20 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.constants.PlayerAnimationConstants;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
-public class GameController implements EventHandler<KeyEvent>, Initializable{
+public class GameController implements Initializable{
     
     @FXML
     SplitPane splitPane;
@@ -28,17 +31,35 @@ public class GameController implements EventHandler<KeyEvent>, Initializable{
     Label roomLabel, playerLabel, debtValue, debtLabel;
     
     @FXML
-    ImageView playerImage;
+    ImageView playerImage,
+    down0,down1,down2,
+    up0,up1,up2,
+    right0,right1,right2,
+    left0,left1,left2;
 
-    @Override
-    public void handle(KeyEvent event) {
-        // TODO Auto-generated method stub
-        
+    public static void catchKey(KeyCode key) {
+        System.out.println(key.toString() + " has been pressed");
     }
-
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         gridPane.getStyleClass().add("gridLines");
+        down0.setImage(new Image(PlayerAnimationConstants.PLAYER_DOWN0));
+        down1.setImage(new Image(PlayerAnimationConstants.PLAYER_DOWN1));
+        down2.setImage(new Image(PlayerAnimationConstants.PLAYER_DOWN2));
+        
+        up0.setImage(new Image(PlayerAnimationConstants.PLAYER_UP0));
+        up1.setImage(new Image(PlayerAnimationConstants.PLAYER_UP1));
+        up2.setImage(new Image(PlayerAnimationConstants.PLAYER_UP2));
+        
+        right0.setImage(new Image(PlayerAnimationConstants.PLAYER_RIGHT0));
+        right1.setImage(new Image(PlayerAnimationConstants.PLAYER_RIGHT1));
+        right2.setImage(new Image(PlayerAnimationConstants.PLAYER_RIGHT2));
+        
+        left0.setImage(new Image(PlayerAnimationConstants.PLAYER_LEFT0));
+        left1.setImage(new Image(PlayerAnimationConstants.PLAYER_LEFT1));
+        left2.setImage(new Image(PlayerAnimationConstants.PLAYER_LEFT2));
+        
     }
 
 }
